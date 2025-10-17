02f77af6ad92e056c018384e919f20568a8f4932
Narendra Atha Abhinaya <71386909+QAmatAS@users.noreply.github.com>
2025-10-17 14:03:56 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://backend-podes.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
